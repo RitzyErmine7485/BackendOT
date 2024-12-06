f6757639b837d16bb735457946b9b997413213d1
RitzyErmine <pacoosorio03@gmail.com>
2024-12-06 14:23:16 -0600

    get_csv

--- app/routes/get_csv.py
+++ app/routes/get_csv.py
@@ -9,14 +9,13 @@ bp = Blueprint('csv', __name__)
 def get_csv():
     data = request.get_json()
 
-    email = request.email
     file_name = data['file_name']
 
     collection = get_collection('csv_data')
 
-    document = collection.find_one({'email': email, 'file_name': file_name}, {'file_name': 1, 'data': 1})
+    document = collection.find_one({'file_name': file_name}, {'file_name': 1, 'data': 1})
 
     if not document:
-        return jsonify({"error": "File not found for the provided email"}), 404
+        return jsonify({"error": "File not found"}), 404
 
     return jsonify({'file_name': document['file_name'], 'data': document['data']})
